d9af121f1df5f33f36851cc53f4907be3b26446c
maseratimatti <80660424+maseratimatti@users.noreply.github.com>
2022-05-05 13:08:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
+from dash import dcc 
+from dash.dependencies import Input, Output
 import pandas as pd
-import plotly.graph_objects as go
 import numpy as np
-import dash_table
-import pandas as pd
-import plotly_express as px
-import plotly.offline as pyo
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 githubpath = './data/'
 
@@ -16,10 +15,10 @@ df = pd.read_excel(githubpath + "Curvexdashdata.xlsx")
 app = dash.Dash()
 
 app.layout = html.Div([ 
+])
 
 fig = px.line(df, x="Tidspunkt", y="Måling", title="Stress") 
 fig.show()
-])
 
 if __name__ == '__main__':
     app.run_server()
